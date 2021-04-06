1335f61702548557647b411ac356faa0b7ab11cf
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2021-04-06 11:45:04 +0900

    amplify 入れてみるテスト

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Analytics from '@aws-amplify/analytics';
+import Auth from '@aws-amplify/auth';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const amplifyConfig = {
+  Auth: {
+    identityPoolId: 'us-east-1:f1266f96-0352-4237-91d4-3347c480d527',
+    region: 'us-east-1'
+  }
+}
+//Initialize Amplify
+Auth.configure(amplifyConfig);
+
+const analyticsConfig = {
+  AWSPinpoint: {
+        // Amazon Pinpoint App Client ID
+        appId: 'f38158f81d05445daa3b37ad0b6a9257',
+        // Amazon service region
+        region: 'us-west-2',
+        mandatorySignIn: false,
+  }
+}
+
+Analytics.configure(analyticsConfig)
+
+Analytics.record('index');
 
 ReactDOM.render(
   <React.StrictMode>
